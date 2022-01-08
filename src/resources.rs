2d4729e9350655807f93b1d6919a470a739fa568
Ayodeji <ayodejisamuelfakunle@gmail.com>
2022-01-08 22:03:10 +0100

    added animations, events and sound to the sokoban game

--- src/resources.rs
+++ src/resources.rs
@@ -1,9 +1,9 @@
-
-#![allow(unused)]
-fn main() {
-// resources.rs
+use crate::audio::AudioStore;
+use crate::events::Event;
 use ggez::event::KeyCode;
 use specs::World;
+
+use std::{fmt::{self, Display}, time::Duration};
 
 // Resources
 #[derive(Default)]
@@ -12,6 +12,46 @@ pub struct InputQueue {
 }
 
 pub fn register_resources(world: &mut World) {
-    world.insert(InputQueue::default())
+    world.insert(InputQueue::default());
+    world.insert(Gameplay::default());
+    world.insert(Time::default());
+    world.insert(EventQueue::default());
+    world.insert(AudioStore::default());
 }
+
+pub enum GameplayState {
+    Playing,
+    Won,
+}
+
+impl Display for GameplayState {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.write_str(match self {
+            GameplayState::Playing => "Playing",
+            GameplayState::Won => "Won",
+        })?;
+        Ok(())
+    }
+}
+
+impl Default for GameplayState {
+    fn default() -> Self {
+        Self::Playing
+    }
+}
+
+#[derive(Default)]
+pub struct Gameplay {
+    pub state: GameplayState,
+    pub moves_count: u32,
+}
+
+#[derive(Default)]
+pub struct Time {
+    pub delta: Duration,
+}
+
+#[derive(Default)]
+pub struct EventQueue {
+    pub events: Vec<Event>,
 }
